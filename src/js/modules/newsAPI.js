f0b05a23ead4e8baa003bfa66c70654fd87846aa
grypis <grypisnecros@gmail.com>
2025-04-05 14:45:27 +0300

    df

--- src/js/modules/newsAPI.js
+++ src/js/modules/newsAPI.js
@@ -1,1 +1,10 @@
 import axios from 'axios';
+
+const articlesApi = axios.create({
+  baseUrl: 'https://newsapi.org/v2/',
+  headers: {
+    'X-Api-Key': '13fd58a9941f4ecbbb0e5d0dbbde037c',
+  },
+});
+
+export function getArticles() {}
